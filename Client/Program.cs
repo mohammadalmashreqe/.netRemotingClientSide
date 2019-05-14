1c9998d031abe926a5e4e6e875a0a9399ebe3249
mohammadalmashreqe <mohammadalmashreqe@yahoo.com>
2019-05-14 16:19:30 +0300

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Http;
+using System.Text;
+using System.Threading.Tasks;
+using TicketServer;
+
+namespace Client
+{
+    
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            HttpChannel httpChannel = new HttpChannel();
+            ChannelServices.RegisterChannel(httpChannel);
+
+            Type requiredType = typeof(MovieTicketInterface);
+
+            MovieTicketInterface remoteObject = (MovieTicketInterface)Activator.GetObject(requiredType,
+                "Http://localhost:9998/MovieTicketBooking");
+
+            Console.WriteLine(remoteObject.GetTicketStatus("Ticket No: 3344"));
+        }
+    }
+    }
+
